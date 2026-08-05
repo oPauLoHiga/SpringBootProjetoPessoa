fea07997f10c848613a9cf8bb1cafe6aac7907e5
Paulo Higa <paulojhiga@gmail.com>
2026-08-05 20:17:40 -0300

    Update PessoaResponse.java

--- src/main/java/com/empresa/cadrastro_pessoas/pessoas/dto/PessoaResponse.java
+++ src/main/java/com/empresa/cadrastro_pessoas/pessoas/dto/PessoaResponse.java
@@ -3,9 +3,13 @@ package com.empresa.cadrastro_pessoas.pessoas.dto;
 import com.empresa.cadrastro_pessoas.pessoas.model.Pessoa;
 import lombok.Getter;
 
+import java.time.LocalDate;
+
 @Getter
 public class PessoaResponse {
     private Long id;
+    private String cpf;
+    private LocalDate dataNascimento;
     private String nome;
     private String email;
     private String telefone;
@@ -19,13 +23,15 @@ public class PessoaResponse {
     public static PessoaResponse de(Pessoa p) {
         PessoaResponse r = new PessoaResponse();
 
-        r.id = p.getId();
-        r.nome = p.getNome();
-        r.email = p.getEmail();
-        r.telefone = p.getTelefone();
-        r.cidade = p.getCidade();
-        r.estado = p.getEstado();
-        r.ativo = p.getAtivo();
+            r.id = p.getId();
+            r.cpf = p.getCpf();
+            r.nome = p.getNome();
+            r.dataNascimento = p.getDataNascimento();
+            r.email = p.getEmail();
+            r.telefone = p.getTelefone();
+            r.cidade = p.getCidade();
+            r.estado = p.getEstado();
+            r.ativo = p.getAtivo();
 
         r.tipoAcessoId = p.getTipoAcesso() != null
                 ? p.getTipoAcesso().getId()
